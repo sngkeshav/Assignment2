1cbda6c5eb2afd976616df7f489b7ae499099540
hrithik-infinite <hrithikinfinite@gmail.com>
2020-05-23 12:37:58 +0530

    assignment 2 solution

---
+++ cachematrix.R
@@ -0,0 +1,38 @@
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
+
+makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(solve) m <<- solve
+  getinv <- function() m
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
+
+}
+
+
+## This function is almost same as mean function as describes in assignment exmaple
+## it takes inv and stores and looks for it in cache when called again instead of 
+## solving it again
+
+cacheSolve <- function(x, ...) {
+        
+  m <- x$getinv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinv(m)
+  m
+
+  }
